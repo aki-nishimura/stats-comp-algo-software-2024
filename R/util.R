0c4175fa0401d652c6054a3b7688d940a0bb4077
Aki Nishimura <aki.nishimura@jhu.edu>
2023-02-06 21:40:59 -0500

    Typo in util Rscript

--- R/util.R
+++ R/util.R
@@ -1,5 +1,5 @@
 install_and_load_packages <- function(package_names) {
-  for (pkg in required_packages) {
+  for (pkg in package_names) {
     if (!(pkg %in% rownames(installed.packages()))) {
       install.packages(pkg)
     }
