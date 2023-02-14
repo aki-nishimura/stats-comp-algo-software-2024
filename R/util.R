a948f43c05bd3838989a6583d185ba97d6826297
Aki Nishimura <aki.nishimura@jhu.edu>
2023-02-13 23:01:40 -0500

    Add utility func to compare two numeric vectors

--- R/util.R
+++ R/util.R
@@ -6,3 +6,10 @@ install_and_load_packages <- function(package_names) {
     library(pkg, character.only = TRUE)
   }
 }
+
+are_all_close <- function(v, w, abs_tol = 1e-6, rel_tol = 1e-6) {
+  abs_diff <- abs(v - w)
+  are_all_within_atol <- all(abs_diff < abs_tol)
+  are_all_within_rtol <- all(abs_diff < rel_tol * pmax(abs(v), abs(w)))
+  return(are_all_within_atol && are_all_within_rtol)
+}
