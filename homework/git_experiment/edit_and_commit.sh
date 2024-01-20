f343c4a20011fcbdb817a209885c9c0ca69e2f3d
Aki Nishimura <aki.nishimura@jhu.edu>
2024-01-20 18:26:33 -0500

    Specify sentence to be added to tex file

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -5,5 +5,6 @@ cd "${HOME}/stats-comp-algo-software-2023/homework/git_experiment_copy"
 for commit_count in {2..10}
 do
   let linenum="10+$commit_count"
+  sentence_to_add="This is Sentence $commit_count."
   # Fill in
 done
