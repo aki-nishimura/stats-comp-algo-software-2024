60553110351b317794c79bf88a0666520e3f92a3
Aki Nishimura <aki.nishimura@jhu.edu>
2024-01-20 13:14:19 -0500

    Update git experiment part of HW1

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-for commit_count in {2..100}
+cd "${HOME}/stats-comp-algo-software-2023/homework/git_experiment_copy"
+
+for commit_count in {2..10}
 do
   let linenum="10+$commit_count"
   # Fill in
