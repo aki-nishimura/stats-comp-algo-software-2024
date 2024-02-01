cc9ffc4356c452cbe728232321ce7a721f54be2e
thepianistalex <llang7@jh.edu>
2024-02-01 00:06:38 -0500

    Finished Exercise 1 Part B

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -2,9 +2,15 @@
 
 cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
 
-for commit_count in {2..10}
+module load tex
+
+for commit_count in {2..10000}
 do
   let linenum="10+$commit_count"
   sentence_to_add="This is Sentence $commit_count."
-  # Fill in
+  sed -i "$linenum i $sentence_to_add" foo.tex
+  pdflatex foo.tex
+  git add foo.tex
+  git commit -m "TeX file change attempt number $commit_count" 
+  du -s -k . | awk '{print $1}' >> git_repo_size_history.txt
 done
