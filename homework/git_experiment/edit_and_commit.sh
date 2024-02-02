f505847ae13ee09e50f05f609d39824ce28608cb
Sam Fansler <sfansle1@jh.edu>
2024-02-01 20:14:33 -0500

    Update edit_and_commit.sh
    Edit edit_and_commit.sh file for assignment

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -2,9 +2,12 @@
 
 cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
 
-for commit_count in {2..10}
+for commit_count in {2..10000}
 do
   let linenum="10+$commit_count"
   sentence_to_add="This is Sentence $commit_count."
-  # Fill in
+  sed -i "$linenum i $sentence_to_add" foo.tex
+  git add foo.tex
+  git commit -m "Add sentence $commit_count to TeX file"
+  du -s -k . >> git_repo_size_history.txt | awk '{print $1}'
 done
