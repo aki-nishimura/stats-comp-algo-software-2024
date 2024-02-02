389ac8fd532000efca6d78e518f7320362a790c4
Michelle Qin <55001348+m-qin@users.noreply.github.com>
2024-02-02 11:43:00 -0500

    start hw1 exercise 1

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+module load tex
+
 cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
 
-for commit_count in {2..10}
+for commit_count in {2..10000}
 do
   let linenum="10+$commit_count"
   sentence_to_add="This is Sentence $commit_count."
-  # Fill in
+  sed -i "$linenum i $sentence_to_add" foo.tex
+  pdflatex foo.tex > /dev/null
+  git add foo.tex
+  git commit -m "Add sentence #$commit_count to TeX file"
+  du -s -k . | awk '{print $1}' >> git_repo_size_history.txt
 done
