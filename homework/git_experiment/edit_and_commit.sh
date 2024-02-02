2ded0284a87ae050d10fae451a34f0c987335c3a
Anagh Chattopadhyay <anagh72@gmail.com>
2024-02-02 17:02:31 -0500

    Edit_and_commit.sh file is updated

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,10 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
+# Set initial line number
+linenum=10
 
-for commit_count in {2..10}
+# Loop for 10000 times
+for ((i=1; i<=10000; i++))
 do
-  let linenum="10+$commit_count"
-  sentence_to_add="This is Sentence $commit_count."
-  # Fill in
+    # Add a new sentence to foo.tex using sed
+    sed -i "$linenum i This is sentence number $i." foo.tex
+
+    # Compile foo.tex 
+    module load tex
+    pdflatex foo.tex > foo.pdf
+
+    # Commit changes to the repository excluding foo.pdf
+    git add foo.tex
+    git commit -m "Add sentence $i to TeX file"
+
+    # Increment line number for the next iteration
+    linenum=$((linenum + 1))
+    
+    # Record the size of the repository in git_repo_size_history.txt
+    du -s -k . | awk '{print $1}' >> git_repo_size_history.txt
 done
