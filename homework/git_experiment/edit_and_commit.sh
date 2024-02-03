be64a98ae0ced25d5c6f4504aafe29889abf715d
Alyssa Columbus <21285656+acolum@users.noreply.github.com>
2024-02-02 22:39:10 -0500

    Edit and commit edit_and_commit.sh

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+module load tex
+
 cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
 
+# Add repo size for the first commit
+du -s -k . | awk '{print $1}' >> git_repo_size_history.txt
+
+# Add repo size for the 2...10000 commits
 for commit_count in {2..10}
 do
   let linenum="10+$commit_count"
   sentence_to_add="This is Sentence $commit_count."
-  # Fill in
+  sed -i "$linenum i $sentence_to_add" foo.tex
+  pdflatex foo.tex > /dev/null
+  git add foo.tex
+  git commit -m "Insert sentence $commit_count in the TeX file"
+  du -s -k . | awk '{print $1}' >> git_repo_size_history.txt
 done
