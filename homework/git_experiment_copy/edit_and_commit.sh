6b6dd4d2417210025da778f2c472a797806cf482
Cindy Fang <cindyfang70@gmail.com>
2024-02-06 14:09:19 -0500

    create git_experiment_copy

---
+++ homework/git_experiment_copy/edit_and_commit.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
+
+for commit_count in {2..10}
+do
+  let linenum="10+$commit_count"
+  sentence_to_add="This is Sentence $commit_count."
+  # Fill in
+done
