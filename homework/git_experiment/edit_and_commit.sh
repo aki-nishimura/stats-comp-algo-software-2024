43b17529cb8b070b800b8db08942e6bee0a81a3e
cindyfang70 <cindyfang70@gmail.com>
2024-02-06 16:11:30 -0500

    move contents of git_experiment_copy into git_experiment

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -2,9 +2,13 @@
 
 cd "${HOME}/stats-comp-algo-software-2024/homework/git_experiment_copy"
 
-for commit_count in {2..10}
+for commit_count in {2..10000}
 do
   let linenum="10+$commit_count"
   sentence_to_add="This is Sentence $commit_count."
   # Fill in
+  sed -i "$linenum i $sentence_to_add" foo.tex
+  git add -u
+  git commit -m "Add sentence $commit_count"
+  du -s -k . | awk '{print $1}' >> git_repo_size_history.txt
 done
