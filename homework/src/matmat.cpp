f71142dee3c932f2068c72c6a8d2922abb6bfb36
Wenxuan Lu <wenxuanlu@hotmail.com>
2024-03-06 22:44:54 -0500

    Exercise 2 done

--- homework/src/matmat.cpp
+++ homework/src/matmat.cpp
@@ -13,7 +13,15 @@ NumericMatrix row_col_dot_matmat(
   int n_row_out = A.nrow();
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
-  // Fill in; remember to use (,) instead of [,] for accessing matrix elements
+  for (int i = 0; i < n_row_out; ++i) {
+    for (int j = 0; j < n_col_out; ++j) {
+      double sum = 0;
+      for (int k = 0; k < A.ncol(); ++k) {
+        sum += A(i, k) * B(k, j);
+      }
+      result(i, j) = sum;
+    }
+  }
   return result;
 }
 
@@ -28,8 +36,11 @@ NumericMatrix col_oriented_matmat(
   int n_col_out = B.ncol();
   NumericMatrix result(n_row_out, n_col_out);
   for (int j = 0; j < n_col_out; ++j) {
-    // Calculate result[, j] = A %*% B[, j] in column-oriented manner for each j
-    // Fill in
+    for (int k = 0; k < A.ncol(); ++k) {
+      for (int i = 0; i < n_row_out; ++i) {
+        result(i, j) += A(i, k) * B(k, j);
+      }
+    }
   }
   return result;
 }
