dd77ab8d0e8cfc4315ca9e55cea83894bb5df830
Cindy Fang <cindyfang70@gmail.com>
2024-03-08 14:22:28 -0500

    add hw5

--- homework/src/axpy_c.cpp
+++ homework/src/axpy_c.cpp
@@ -3,6 +3,11 @@ using Rcpp::NumericVector;
 
 // [[Rcpp::export]]
 NumericVector axpy_c(double a, NumericVector& x, NumericVector& y) {
+
+  if (x.size() >  y.size()){
+      Rcpp::stop("Incompatible dimensions");
+  }
+  
   int n = x.size();
   for (int i = 0; i < n; ++i) {
     y[i] += a * x[i];
