1ef68837805b68119351430228a2e889d8aea2d8
achatto4 <149892725+achatto4@users.noreply.github.com>
2024-03-08 21:35:57 -0500

    Update gemv_eigen.cpp

--- homework/src/gemv_eigen.cpp
+++ homework/src/gemv_eigen.cpp
@@ -8,8 +8,8 @@ using Eigen::VectorXd;
 // [[Rcpp::export]]
 VectorXd dgemv_eigen(Map<MatrixXd> A, Map<VectorXd> v) {
   int n_col_left_mat = A.cols();
-  int n_row_right_mat = A.rows(); // Corrected the variable name to get the number of rows of A
-  if (n_col_left_mat != v.size()) { // Changed to check the size of the vector v
+  int n_row_right_mat = A.rows();
+  if (n_col_left_mat != v.size()) { 
     Rcpp::stop("Incompatible matrix and vector dimensions.");
   }
   return A * v;
