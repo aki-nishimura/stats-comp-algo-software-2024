0ec240f97a2e4a078fddf0263a1b96c8e4d7dde2
alepigna12 <70845868+alepigna12@users.noreply.github.com>
2024-03-09 21:35:36 -0500

    axpy_c with error checking (part 1c)

--- homework/src/axpy_c.cpp
+++ homework/src/axpy_c.cpp
@@ -4,6 +4,9 @@ using Rcpp::NumericVector;
 // [[Rcpp::export]]
 NumericVector axpy_c(double a, NumericVector& x, NumericVector& y) {
   int n = x.size();
+  if (n != y.size()) {
+    Rcpp::stop("Incompatible vector lengths.");
+  }
   for (int i = 0; i < n; ++i) {
     y[i] += a * x[i];
   }
